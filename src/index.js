80b9b31e7f42fb5d477e2e1ac4e8d1f3a8911bbe
Vansh Rathor <vanshrathor5887@gmail.com>
2026-06-21 17:27:17 +0530

    feat: add main entry point with all validator exports

---
+++ src/index.js
@@ -0,0 +1,4 @@
+export { validatePAN } from './validators/pan.js';
+export { validateAadhaar } from './validators/aadhaar.js';
+export { validateGST } from './validators/gst.js';
+export { validateIFSC } from './validators/ifsc.js';
